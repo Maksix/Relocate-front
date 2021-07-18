f5d3c2db48f1be6301c1542909db9939f76909e7
Суржиков Максим Леонидович <MLeSurzhikov@sberbank.ru>
2021-07-18 21:22:33 +0300

    added modal for filters

--- front/src/components/AccommodationListPage/AccommodationListPage.tsx
+++ front/src/components/AccommodationListPage/AccommodationListPage.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd';
+
 import IPage from '../../interfaces/IPage';
 import Breadcrumbs from '../../common/Breadcrumbs/Breadcrumbs';
 import FiltersBlock from './FiltersBlock/FiltersBlock';
@@ -9,18 +11,32 @@ import MapToggleBlock from './MapToggleBlock/MapToggleBlock';
 
 const AccommodationListPage: React.FC<IPage> = () => {
   const breadcrumbs = [{ text: 'Главная', link: '/' }, { text: 'Поиск' }];
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="list-page">
       <div className="list-header">
         <Breadcrumbs breadcrumbs={breadcrumbs} />
+        <div className="filter-button">
+          <Button onClick={() => setVisible(true)}>
+            Фильтры
+          </Button>
+        </div>
+        <Modal
+          visible={visible}
+          centered
+          width={400}
+          onOk={() => setVisible(false)}
+          onCancel={() => setVisible(false)}
+        >
+          <FiltersBlock />
+        </Modal>
         <SortBlock />
       </div>
       <div className="list-body">
         <div className="filter-menu-block">
           <MapToggleBlock />
-          <div>
-            <FiltersBlock />
-          </div>
+          <FiltersBlock />
         </div>
         <AccommodationList />
       </div>
